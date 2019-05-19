237d3a7247c315541f4b766a9c5cc86b2d59cf77
hopfKO <tuyuhiko@gmail.com>
2019-05-19 23:02:14 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: '練習用サイト' });
+  res.render('index', { title: 'Test' });
 });
 
 module.exports = router;
