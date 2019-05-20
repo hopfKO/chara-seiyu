95e7a372f83d27f6ee1619fa500b66264ea0b71b
hopfKO <tuyuhiko@gmail.com>
2019-05-20 23:10:09 +0900

    appli

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'アプリ' });
+  res.render('index', { title: 'appli' });
 });
 
 module.exports = router;
