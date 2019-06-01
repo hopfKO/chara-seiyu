72046fac81e63a2d0d7134e4bf0d8ef7b815da7d
hopfKO <tuyuhiko@gmail.com>
2019-06-01 15:40:55 +0900

    slack

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'appli' });
+  res.render('index', { title: 'slack' });
 });
 
 module.exports = router;
