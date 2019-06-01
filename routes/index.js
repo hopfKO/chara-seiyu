3fac25f97d1d8be99404a8c15685a1096fa60fab
hopfKO <tuyuhiko@gmail.com>
2019-06-01 16:06:12 +0900

    ff

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'slack' });
+  res.render('index', { title: 'jenkins' });
 });
 
 module.exports = router;
